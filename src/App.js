ffca63389fa5fa5b243bb10679bbbd8c7addc1f3
Yaakov Holdowsky <yholdowsky@aeiginsurance.com>
2024-09-23 10:40:23 -0400

    ss

--- src/App.js
+++ src/App.js
@@ -1,43 +1,58 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 import "./App.css";
 import Header from "./components/Header";
-// import Footer from "./components/Footer";
-import HomePage from "./components/HomePage";
-import ContactPage from "./components/ContactPage";
-import ServicesPage from "./components/ServicesPage";
-import CustomWebDesignServiceDetails from "./components/CustomWebDesignServiceDetails";
-import AboutPage from "./components/AboutPage";
-import Portfolio from "./components/Portfolio";
 import Footer from "./components/Footer";
-import ScrollToTop from "./components/ScrollToTop"; // Import ScrollToTop component
+import ScrollToTop from "./components/ScrollToTop";
 import { Analytics } from "@vercel/analytics/react";
+import { Helmet } from "react-helmet";
+
+// Lazy loading components
+const HomePage = lazy(() => import("./components/HomePage"));
+const ContactPage = lazy(() => import("./components/ContactPage"));
+const ServicesPage = lazy(() => import("./components/ServicesPage"));
+const CustomWebDesignServiceDetails = lazy(() =>
+  import("./components/CustomWebDesignServiceDetails")
+);
+const AboutPage = lazy(() => import("./components/AboutPage"));
+const Portfolio = lazy(() => import("./components/Portfolio"));
 
 function App() {
   return (
-    <Router>
-      <ScrollToTop /> {/* Add ScrollToTop component */}
-      <ToastContainer />
+    <>
+      <Helmet>
+        <title>My App - Web Development and Design</title>
+        <meta
+          name="description"
+          content="Web development and design services."
+        />
+      </Helmet>
+      <ToastContainer /> {/* Moved outside Router */}
       <Analytics />
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/services" element={<ServicesPage />} />
-          <Route
-            path="/services/custom-website-design"
-            element={<CustomWebDesignServiceDetails />}
-          />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+      <Router>
+        <ScrollToTop />
+        <div className="App">
+          <Header />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/services" element={<ServicesPage />} />
+              <Route
+                path="/services/custom-website-design"
+                element={<CustomWebDesignServiceDetails />}
+              />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/contact" element={<ContactPage />} />
+              <Route path="/portfolio" element={<Portfolio />} />
+              <Route path="*" element={<div>Page Not Found</div>} />
+            </Routes>
+          </Suspense>
+          <Footer />
+        </div>
+      </Router>
+    </>
   );
 }
 
