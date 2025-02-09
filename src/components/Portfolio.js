1b01e62bb9045c977d48d3612c3bb90dd0c9e898
Jacob Holdowsky <jacobedward1995@gmail.com>
2025-02-09 17:39:26 +0200

    added power coffeeworks to portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from "react";
 import "./Portfolio.css";
 import { FaExternalLinkAlt } from "react-icons/fa";
+import { Coffee } from 'lucide-react';
 
 function PortfolioPage() {
   const [modalOpen, setModalOpen] = useState(false);
@@ -14,6 +15,12 @@ function PortfolioPage() {
       websiteURL: "https://tandbeeliquidgold.com",
       description:
         "Developed a sophisticated e-commerce platform for T&Bee Liquid Gold, a premium honey brand. The website offers a seamless shopping experience with integrated Stripe payments for secure transactions, AWS-based image storage for optimal performance, and real-time currency conversion for a global audience. The refined UX/UI ensures an intuitive, engaging customer journey from browsing to checkout.",
+    },
+    "Power CoffeeWorks": {
+      imageComponent: < Coffee className="portfolio-image" />,
+      websiteURL: "https://gregarious-trifle-73e06e.netlify.app/",
+      description:
+        "Developed a modern, feature-rich e-commerce platform for Power CoffeeWorks, offering a seamless shopping experience with a robust set of functionalities. The site includes a fully integrated shopping cart, a user-friendly contact form, and a comprehensive admin console for managing products, tracking orders, and updating website content in real time. Customers can switch effortlessly between English and Hebrew with a single click, ensuring accessibility for a broader audience.",
     },
     "Roots Rockland": {
       imageURL: "rootsLogo2.png",
@@ -57,34 +64,36 @@ function PortfolioPage() {
       <div className="portfolio-container">
         <h1 className="portfolio-title">Our Portfolio</h1>
         <div className="portfolio-grid">
-          {Object.entries(portfolioDetails).map(
-            ([projectName, projectData]) => (
-              <div className="portfolio-card" key={projectName}>
+          {Object.entries(portfolioDetails).map(([projectName, projectData]) => (
+            <div className="portfolio-card" key={projectName}>
+              {projectData.imageURL ? ( // If it's an image URL, render an img tag
                 <img
                   src={projectData.imageURL}
                   alt={projectName}
                   className="portfolio-image"
                   onClick={() => openModal({ projectName, ...projectData })}
                 />
-                <div className="portfolio-card-content">
-                  <h3 className="portfolio-card-title">{projectName}</h3>
-                  
-                  <p className="portfolio-card-description">
-                    {projectData.description.length > 100
-                      ? projectData.description.substring(0, 100) + "..."
-                      : projectData.description}
-                  </p>
-                  <button
-                    onClick={() => openModal({ projectName, ...projectData })}
-                    className="portfolio-btn"
-                  >
-                    View Details
-                  </button>
+              ) : (
+                projectData.imageComponent // If it's JSX, render it directly
+              )}
 
-                </div>
+              <div className="portfolio-card-content">
+                <h3 className="portfolio-card-title">{projectName}</h3>
+                <p className="portfolio-card-description">
+                  {projectData.description.length > 100
+                    ? projectData.description.substring(0, 100) + "..."
+                    : projectData.description}
+                </p>
+                <button
+                  onClick={() => openModal({ projectName, ...projectData })}
+                  className="portfolio-btn"
+                >
+                  View Details
+                </button>
               </div>
-            )
-          )}
+            </div>
+          ))}
+
         </div>
       </div>
 
@@ -100,11 +109,15 @@ function PortfolioPage() {
             </button>
             <div className="modal-content">
               <h2 className="modal-title">{selectedProject.projectName}</h2>
-              <img
-                src={selectedProject.imageURL}
-                alt={selectedProject.projectName}
-                className="modal-image"
-              />
+              {selectedProject.imageURL ? ( // If it's an image URL, render an img tag
+                <img
+                  src={selectedProject.imageURL}
+                  alt={selectedProject.projectName}
+                  className="portfolio-image"
+                />
+              ) : (
+                selectedProject.imageComponent // If it's JSX, render it directly
+              )}
               <p className="modal-description">{selectedProject.description}</p>
               <div className="modal-buttons">
                 <a
